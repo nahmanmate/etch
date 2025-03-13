6e8933faa24952d5acbfedcd4e91eb3ad8d8fd1d
nahmanmate <192176108+nahmanmate@users.noreply.github.com>
2025-03-14 00:19:02 +1100

    Improved styling

--- main.js
+++ main.js
@@ -7,4 +7,8 @@ Create loop to create a series of 8 divs within each row
 Add styling to all divs
     flex: 1 1 auto;
     
-Create event delegation fucntion to add :
+Create event delegation fucntion to add :hover and change color - add new class
+create a button at top of the screen that prompts for number of squares per side
+remove existing grid
+generate new grid (set max of 100)
+keep same overall size
