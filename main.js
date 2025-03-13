48118a99143eb5c1918599209fef307579f20654
nahmanmate <192176108+nahmanmate@users.noreply.github.com>
2025-03-13 22:18:20 +1100

    Added partial pseudocode

---
+++ main.js
@@ -0,0 +1,10 @@
+Create a gridContainer
+add styling to gridContainer
+    flex: 1 1 auto;
+Create loop to create a series of 8 divs (rows)
+Add styling to row divs using flex row
+Create loop to create a series of 8 divs within each row
+Add styling to all divs
+    flex: 1 1 auto;
+    
+Create event delegation fucntion to add :
